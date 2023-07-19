f7377eebfb0784f0a5d4cb118d6c239ad0083bbe
Emmy-ck <ckenduiwa9876@gmail.com>
2023-07-19 05:29:07 +0300

    The last digit

--- 3-last_digit.py
+++ 3-last_digit.py
@@ -1,11 +1,19 @@
 import random
 number = random.randint(-10000, 10000)
-x = number % 10 if number >= 0 else -(-number % 10)
-output = ""
-if x == 0:
-    output = "and is 0"
-elif x > 5:
-    output = "and is greater than 5"
-else:
-    output = "and is less than 6 and not 0"
-print("Last digit of {} is {} {}".format(number, x, output), end="\n")
+
+y = str(number)
+x = int(y[-1])
+
+if number > 0:
+    if x > 5:
+        print(f"Last digit of {number} is {x} and is greater than 5")
+    elif x < 6 and x != 0:
+        print(f"Last digit of {number} is {x} and is less than 6 and not 0")
+    elif x == 0:
+        print(f"Last digit of {number} is {x} and is 0")
+elif number < 0:
+    if x == 0:
+        print(f"Last digit of {number} is {x} and is 0")
+    else:
+        x = -(x)
+        print(f"Last digit of {number} is {x} and is less than 6 and not 0")
