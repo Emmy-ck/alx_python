f39d1599ff9712ceed3ddc6003f47143d9e2181b
Emmy-ck <ckenduiwa9876@gmail.com>
2023-07-19 04:54:46 +0300

    Minor fix

--- 3-last_digit.py
+++ 3-last_digit.py
@@ -6,20 +6,15 @@ x = int(y[-1])
 
 if number > 0:
     if x > 5:
-        x = str(x)
         print(f"Last digit of {number} is {x} and is greater than 5")
     elif x < 6 and x != 0:
-        x = str(x)
         print(f"Last digit of {number} is {x} and is less than 6 and not 0")
     elif x == 0:
-        x = str(x)
         print(f"Last digit of {number} is {x} and is 0")
 elif number < 0:
     if x == 0:
-        x = str(x)
         print(f"Last digit of {number} is {x} and is 0")
     else:
         x = -(x)
-        x = str(x)
         print(f"Last digit of {number} is {x} and is less than 6 and not 0")
         
