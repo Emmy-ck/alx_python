4cca126bba3d57d13ba284e372aa0a6b00ab68d0
Emmy-ck <ckenduiwa9876@gmail.com>
2023-07-25 00:48:13 +0300

    Prime Number Function

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    
+    for i in range(2, int(number ** 0.5) + 1): #Check divisors from 2 upto the squareroot of the number
+        if number % i == 0:
+            return False
+        
+    return True
