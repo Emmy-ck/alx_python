868515676839d1b5bf2c7f9d7cccc022048e0d00
Emmy-ck <ckenduiwa9876@gmail.com>
2023-07-26 05:48:49 +0300

    Minor fixes

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,6 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
+        return
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
