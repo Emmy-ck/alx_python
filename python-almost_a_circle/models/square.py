6bf942680b626d0dae1397aa5f1e9c88ca48ece3
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-10 06:17:59 +0300

    And now, the Square!

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Square Module
+"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    Square class that inherits from the Recctangle class.
+    
+    """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initialize a Square instance
+
+        Args:
+            size (int): size of the square
+            x (int): x-coordinate position of the square. Defaults to 0.
+            y (int): y-coordinate position of the square. Defaults to 0.
+            id (int): id for the instance. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+        
+    def __str__(self):
+        """ 
+        Returns the string representation of the Square
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+    
+if __name__ == "__main__":
+    pass
