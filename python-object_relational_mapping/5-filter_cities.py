52f968a85e3110c06410ab017cc1c7d473d928ac
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-23 05:01:35 +0300

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-
 import MySQLdb
 import sys
 
 
-def list_cities(username, password, database, city_name):
+def list_cities(username, password, database, state_name):
     """Connect to a MySQL server and lists all states
     where name matches the argument
 
@@ -12,7 +10,7 @@ def list_cities(username, password, database, city_name):
         username (str): MySQL username
         password (str): MySQL password
         database (str): Name of database to conect to
-        city_name (str): City name listed
+        state_name (str): Name of state
     """
     try:
         # Connecting to the MySQL server
@@ -26,14 +24,12 @@ def list_cities(username, password, database, city_name):
         # Create a cursor object to interact with the data
         cursor = db.cursor()
 
-        # Excecute a query that takes name of state as an argument
-        # and lists all cities of the state
-        safe = ("SELECT cities.name "
-                "FROM cities "
-                "WHERE cities.states_id = (SELECT id FROM states WHERE name = %s) "
-                "ORDER BY cities.id ASC")
-        # Safe query using parametized query
-        cursor.excecute(safe, (city_name,))
+        # Excecute query that lists all cities from the states in the db
+        cursor.excecute = ("SELECT cities.name "
+                           "FROM cities "
+                           "JOIN states ON cities.state_id = states.id "
+                           "WHERE states.name = %s "
+                           "ORDER BY cities.id ASC")
         # Fetch all rows as list if tuples
         rows = cursor.fetchall()
 
@@ -47,10 +43,9 @@ def list_cities(username, password, database, city_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: Python script.py <username> <password> <database>"
-              "<cities>")
+        print("Usage: Python script.py <username> <password> <database> <state_name>")
     else:
         username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        city_name = sys.argv[4]
-        list_cities(username, password,
-                    database, city_name)
+        state_name = sys.argv[4]
+        list_cities(username, password, database,
+                    state_name)
