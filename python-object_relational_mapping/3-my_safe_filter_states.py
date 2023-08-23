be441c016642facff554199f96450c88e6b32c57
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-23 04:15:42 +0300

    Error fixed

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -29,7 +29,7 @@ def search_states(username, password, database, state_name):
         # Excecute a safe query to display all values in states table
         # where name matches argument
         safe = ("SELECT * FROM states "
-                       "WHERE name LIKE BINARY = '{}' "
+                       "WHERE BINARY name = '{}' "
                        "ORDER BY states.id ASC")
         # Safe query using parametized query
         cursor.excecute(safe, (state_name,))
