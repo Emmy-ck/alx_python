cbd08b7141c8a68bba8a3498947d61536a074146
Emmy-ck <ckenduiwa9876@gmail.com>
2023-08-23 04:06:20 +0300

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -28,10 +28,10 @@ def search_states(username, password, database, state_name):
 
         # Excecute the SQL query to display all values in states table
         # where name matches
-        cursor.execute("SELECT * FROM states "
-                       "WHERE BINARY name LIKE = '{0}%' "
-                       "ORDER BY states.id ASC").format(state_name)
-
+        query = ("SELECT * FROM states "
+                "WHERE BINARY name LIKE = '{0}%' "
+                "ORDER BY states.id ASC").format(state_name)
+        cursor.execute(query)
         # Fetch all rows as list if tuples
         rows = cursor.fetchall()
 
