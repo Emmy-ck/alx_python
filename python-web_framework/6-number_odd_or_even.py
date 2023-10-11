f900d021886a9ffa961b0f9cac88d5449f3dad94
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-11 18:02:11 +0300

    Fix

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,63 +1,66 @@
-#!/usr/bin/python3
-"""
-This is a script that directs a user to the home page of a site.
-"""
+"""Importing the flask web framework and render_template"""
+
 from flask import Flask, render_template
+
+"""Creating an instance of the Flask class and store it in the 'app' variable."""
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def display():
-    """
-    This function directs visitors to the homepage.
-    """
-    return "Hello HBNB!"
+"""Defining a route for the root URL ('/') and associate it with the 'hello' function
+"""
+
+@app.route('/')
+def hello():
+    """ Return the string 'Hello HBNB!' as the response when the route is accessed."""
+
+    return 'Hello HBNB!'
+
+"""Defining a route for the hbnb URL and associate ti with the 'helloroute' function"""
 
 @app.route('/hbnb')
-def display2():
-    """
-    This function directs visitors to the HBNB page.
-    """
-    return "HBNB"
+def helloroute():
+    """Return the string output as a response when this route is accessed"""
+    return 'HBNB'
 
-@app.route('/c/<good>', strict_slashes=False)
-def display3(good):
-    """
-    This function returns the values of a dynamic route using flask!
-    """
-    good = good.replace('_', " ")
-    return "C {}".format((good))
+"""Defining a route for the c URL and associate it eith the 'disp_text' function with one argument 'text'"""
 
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', defaults={'text': 'is_cool'})
-def display4(text):
-    """
-    This function displays a default text.
-    """
-    text = text.replace('_', " ")
-    return "Python {}".format((text))
+@app.route('/c/<text>')
+def disp_text(text):
+    """Replacing the _ with space if underscore is present in the route"""
+    new_text = text.replace('_',' ')
+    """Returns the concatnated new string without underscores"""
+    return 'C ' + new_text
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display5(n):
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+"""Defining a route for the python URL and associate it with the 'py_text' function."""
+"""This dynamic route has a default text value"""
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text='is cool'):
+    """Replaces the underscore with a space"""
+    new_text1 = text.replace('_',' ')
+    """Returns the new string"""
+    return 'Python ' + new_text1
 
+"""Defining a route for the number URL and associating it with the 'num' function."""
+@app.route('/number/<int:n>')
+def num(n):
+    """Converts int n into a string for the output"""
+    m = str(n)
+    """Displays a number only if the number is an integer."""
+    return m + ' is a number'
+
+"""Defining a new route for the number_template URL and associating it with the 'num_temp' function."""
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Displays the HTML page only if the number is an integer"""
-    try:
-        return render_template('6-number_odd_or_even.html', odd_or_even='even' if n % 2 == 0 else 'odd')
-    except Exception as e:
-        print("Error", e)
+    """Displays the html page only if the number is an integer"""
+    return render_template('5-number.html', number=n)
 
-@app.route('/number_odd_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """Displays the HTML page only depending on whether the number is odd or even"""
-    if isinstance(n, int):
-        if n % 2 == 0:
-            return render_template('6-number_odd_or_even.html', n=n, odd_or_even='even')
-        else:
-            return render_template('6-number_odd_or_even.html', n=n, odd_or_even='odd')
+@app.route("/number_odd_or_even/<int:n>", strict_slashes = False)
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
 
-if __name__ == '__main__':
+"""Check if the string is being executed directly and not imported"""
+
+if __name__ == "__main__":
+    """ Start the Flask development web server with debugging enabled."""
     app.run(host='0.0.0.0', port=5000)
