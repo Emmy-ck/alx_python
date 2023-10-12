424c865a731474b567b1dbbb17055ed06616c1bd
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-12 11:59:03 +0300

    PEP8 validation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,5 @@
 """Start link class to table in the database
 """
-
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
