377ce3cc214bd54a58810ea0f86f59e982e60745
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-12 14:01:09 +0300

    POST an email #1

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+"""Python file that handles email post request
+"""
+# Check if both URL and email are provided as command line argumets
+if len(sys.argv) != 3:
+    print("Usage: python script.py <URL> <email>")
+    sys.exit(1)
+    
+# Get the URL and email from the command line arguments
+url = sys.argv[1]
+email = sys.argv[2]
+
+# Create a dictionary with the email parameter
+data = {'email': email}
+
+# Send a POST request to the provided URL with the email parameter
+response = requests.post(url, data=data)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    print("Response body:")
+    print(response.text)
+else:
+    print(f"Request failed with status code: {response.status_code}")
