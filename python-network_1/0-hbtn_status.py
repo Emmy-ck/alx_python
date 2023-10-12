2e4c59cc6c3c20c14ca0260fe275f8c4c0b220d2
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-12 13:45:03 +0300

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print("Body response:")
-    print("- type:" + str(type(response.text)))
-    print("- content: " + response.text)
+    print("\t- type:" + str(type(response.text)))
+    print("\t- content: " + response.text)
 else:
     print(f"Error: Unable to retrieve status. Status code: {response.status_code}")
