8ab933ce213a078861ad21454c92479e69522be3
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-12 11:11:47 +0300

    Activating sqlalchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in the database
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class State(Base):
+    """Class that represents the states table in the database"""
+    __tablename__ = 'states'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    
+    def __inti__(self, name):
+        self.name = name
