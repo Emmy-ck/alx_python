4d69e54021e693f3f7779a1efe73ba0d07594dd7
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-16 00:01:01 +0300

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,10 @@
 """Empty class BaseGeometry
 """
+from collections.abc import Iterable
+
+
 class BaseGeometry:
     """Empty class that serves as the base for geometry-related classes
     """
-    pass
+    def __dir__(self):
+        return [attr for attr in dir(type(self)) if not attr.startswith('__')]
