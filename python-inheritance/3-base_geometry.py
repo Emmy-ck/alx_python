cf0068a69f1eb42d9b672ee767b48e704eb7e4fa
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-16 16:53:25 +0300

    Fixing task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,12 +1,4 @@
 """Empty class BaseGeometry"""
 class BaseGeometry:
     """Empty class that serves as the base for geometry-related classes"""
-    def __init_subclass__(cls):
-        pass
-
-    def __dir__(self):
-        attributes = super().__dir__()
-        used_attr = [att for att in attributes if att != "__init_subclass__"]
-        return used_attr
-
-BaseGeometry.__dir__ = lambda self: [att for att in super(BaseGeometry, self).__dir__() if att != "__init_subclass__"]
+    pass
