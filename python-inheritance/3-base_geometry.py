e24f089cf92d51e7847b1120c2b7b9c3a13f51cc
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-16 17:20:18 +0300

    Improve Geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Empty class BaseGeometry"""
+"""
+Empty class definition for BaseGeometry.
+"""
+
+
 class BaseGeometry:
-    """Empty class that serves as the base for geometry-related classes"""
+    """
+    Empty class representing the base geometry.
+    """
     def __init_subclass__(cls):
         pass
 
     def __dir__(self):
-        return [att for att in dir(type(self)) if att != "__init_subclass__"]
-    
+        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
+
     def __class__(self):
         return type(self)
-
-BaseGeometry.__dir__ = lambda self: [att for att in super(BaseGeometry, self).__dir__() if att != "__init_subclass__"]
