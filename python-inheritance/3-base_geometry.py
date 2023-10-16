395cb147279698e21fe7ef742d58df550a980890
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-16 16:27:25 +0300

    Fixing task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,25 @@
 """Empty class BaseGeometry
 """
 
-class BaseGeometry:
+from collections.abc import Iterable
+
+
+class BaseGeometry(type):
     """Empty class that serves as the base for geometry-related classes
     """
     def __dir__(self):
-        return [attr for attr in dir(type(self)) if not attr.startswith('__')]
+        attributes = super().__dir__()
+        attr = [att for att in attributes if att != "__init_subclass__"]
+        return attr
+    
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """Primary class for creating geometry-related classes
+
+    Args:
+        metaclass: Defaults to BaseGeometryMeta.
+    """
+    
+    def __dir__(self):
+        attibutes = super().__dir__()
+        attr = [att for att in attributes if att != "__init_subclass__"]
+        return attr
