793eac58fda2ced5d468941c3b02a36eba84e9ff
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-17 21:09:46 +0300

    PEP8 validation check

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-        
+
     # Check a connection to the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     # Query and display State objects sorted by states.id
     states = session.query(State).order_by(State.id).all()
     for state in states:
         print("{}: {}".format(state.id, state.name))
-        
+
     # Close the session
     session.close()
