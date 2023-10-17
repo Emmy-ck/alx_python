499cecf6248fc0283c6c52278d659ccadd1e826c
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-17 21:35:36 +0300

    PEP8 validation

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,22 +12,22 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-        
+
     # Create a connection to the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     # Query and display the first State object
     first_state = session.query(State).order_by(State.id).first()
-    
+
     if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-        
+
     # Close the session
     session.close()
