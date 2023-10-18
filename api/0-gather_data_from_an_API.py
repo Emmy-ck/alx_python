43bedcb8c490836d46ece5faf04e4f7bb4e3b0f1
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-18 10:53:01 +0300

    Fixing errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,43 @@
-import requests, sys
+import csv
+import requests
+import sys
 
-def get_employee_data(employee_id):
-    # URL to fetch employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    # URL to fetch employee TO DO list
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    
-    try:
-        # Fetch employee data
-        response_employee = requests.get(employee_url)
-        response_employee.raise_for_status()
-        employee_data = response_employee.json()
-        
-        # Fetch employee TO DO list
-        response_todos = requests.get(todos_url)
-        response_todos.raise_for_status()
-        todos_data = response_todos.json()
-        
-        return employee_data,todos_data
-    
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-        
 def main():
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
-        
-    employee_id = int(sys.argv[1])
-    employee_data, todos_data = get_employee_data(employee_id)
+
+    employee_id = int(sys.argv[1]
+
+    # Fetch employee data
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response_employee = requests.get(employee_url)
     
-    # Extract employee information
-    employee_name = employee_data.get("name")
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo["completed"])
+    if response_employee.status_code != 200:
+        print(f"Employee with ID {employee_id} not found.")
+        return
     
-    # Print employee TO DO list progress
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+    employee_data = response_employee.json()
+
+    # Fetch employee TO DO list
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response_todos = requests.get(todos_url)
     
-    # Print titles of completed tasks
-    for todo in todos_data:
-        if todo["completed"]:
-            print(f"\t {todo['title']}\n")
+    if response_todos.status_code != 200:
+        print(f"Failed to fetch todos for employee {employee_data['name']}.")
+        return
+
+    todos_data = response_todos.json()
+
+    # CSV file
+    csv_file = f"{employee_id}.csv"
+    with open(csv_file, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            csv_writer.writerow([employee_id, employee_data['name'], task['completed'], task['title']])
+
+    print(f"Data exported to {csv_file}.")
+
 
 if __name__ == "__main__":
     main()
-    
