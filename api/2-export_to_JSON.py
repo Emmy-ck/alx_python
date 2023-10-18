4a1f15908ff024dab35eb8dcc0df364abac5ed61
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-19 01:15:35 +0300

    Fixing errors

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,39 +1,50 @@
 """This program fetches employee data and exports it to both CSV and JSON format
-
 """
-
-import json
+import json  # Import the json module
 import requests
 import sys
 
-if __name__ == '__main__':
+def main():
     employee_id = sys.argv[1]
 
-    # getting user
-    employee_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    employee = requests.get(employee_url)
-    employee_data = employee.json()
+    # Employee data
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_response = requests.get(employee_url)
+    if employee_response.status_code != 200:
+        print(f"Employee with ID {employee_id} not found.")
+        return
+    employee_data = employee_response.json()
 
-    # getting todos
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-    todos = requests.get(todo_url)
-    todos_data = todos.json()
+    # Fetching todos data
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+        print(f"Failed to fetch todos for user {employee_data['name']}.")
+        return
+    todos_data = todos_response.json()
 
-    # exporting to a json
-    json_file = "{}.json".format(employee_id)
-    
-    employee_info = {
-        f"{employee_id}": [
-            {
-                "task": items['title'],
-                "completed": items['completed'],
-                "username": employee_data['username'],
-            }
+    # Creating a list to store task records
+    task_records = []
 
-            for items in todos_data
-        ]
+    for task in todos_data:
+        task_record = {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employee_data['username']
+        }
+        task_records.append(task_record)
+
+    # Creating a dictionary with the user ID as the key and the list of task records as the value
+    export_data = {
+        employee_id: task_records
     }
 
-    # write data to json file
-    with open(json_file, 'w') as json_files:
-        json.dump(employee_info, json_files)
+    # Exporting to JSON file
+    json_file = f"{employee_id}.json"
+    with open(json_file, 'w') as jsonfile:
+        json.dump(export_data, jsonfile, indent=4)  # Use json.dump to write data in JSON format
+
+    print(f"Data exported to {json_file}.")
+
+if __name__ == "__main":
+    main()
