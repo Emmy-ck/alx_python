f1686d16aaeac3fb506e735f076321eb60e5c3c2
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-18 13:40:32 +0300

    Fixing task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,5 +4,7 @@ Empty class definition for BaseGeometry.
 
 
 class BaseGeometry:
+    """Empty class representing the base geometry
+    """
     def area(self):
         raise Exception("area() is not implemented")
