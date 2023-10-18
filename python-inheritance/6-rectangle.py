ab6dd0e1200fc82e1fe3699da3dca54a5d7ea0f1
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-19 00:39:18 +0300

    Full rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -13,12 +13,7 @@ class Rectangle(BaseGeometry):
         height (int): The height of the rectangle.
     """
     def __init__(self, width, height):
-        
-        super().__init__()
         self.__width = width
         self.__height = height
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
-        
-    def __str__(self):
-        return f"<{self.__class__.__name__} ({self.__width}, {self.__height})>"
