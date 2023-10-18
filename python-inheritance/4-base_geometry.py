494e8806659bdefb6918bbfe9094da2a25a9fcb3
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-19 00:01:05 +0300

    Error formatting

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""Class BaseGeometry based on 3-base_geometry.py file
-"""
+
 class BaseGeometry:
-    """Base class for geometry-related classes."""
-
-    def __init_subclass__(cls):
-        pass
-
-    def __dir__(self):
-        attributes = super().__dir__()
-        used_attr = [att for att in attributes if att != "__init_subclass__"]
-        return used_attr
-
-BaseGeometry.__dir__ = lambda self: [att for att in super(BaseGeometry, self).__dir__() if att != "__init_subclass__"]
-
-class BaseGeometryWithArea(BaseGeometry):
     def area(self):
+        """
+        Public instance method to calculate the area of the geometry.
+
+        Raises:
+            Exception: This method should be implemented in the derived class.
+        """
         raise Exception("area() is not implemented")
 
-bg = BaseGeometryWithArea()
-
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __dir__(self):
+        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
