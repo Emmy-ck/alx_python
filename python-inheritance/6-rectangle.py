a52daf9d105f58a7f7da514d163c38dbf9b5ff42
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-19 00:16:47 +0300

    Rectangle

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,19 @@
+"""Class rectangle that inherits from BaseGeometry class
+"""
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Initialize a Rectangle with width and height.
+
+    Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
