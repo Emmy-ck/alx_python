8e03a4ba7ef1208c4a995dab4ee509fcd7f74ae7
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-19 01:04:38 +0300

    Fixing errors

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,8 +2,11 @@ import csv
 import requests
 import sys
 
-
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        return
+
     employee_id = sys.argv[1]
 
     # Employee data
@@ -15,7 +18,7 @@ def main():
     employee_data = employee_response.json()
 
     # Fetching todos data
-    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     todos_response = requests.get(todos_url)
     if todos_response.status_code != 200:
         print(f"Failed to fetch todos for user {employee_data['name']}.")
@@ -26,11 +29,11 @@ def main():
     csv_file = f"{employee_id}.csv"
     with open(csv_file, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for task in todos_data:
-            csv_writer.writerow(
-                [employee_id, employee_data['username'], task['completed'], task['title']])
+            csv_writer.writerow([employee_id], employee_data['username'], task['completed'], task['title'])
 
-    # print(f"Data exported to {csv_file}.")
+    print(f"Data exported to {csv_file}.")
 
-if __name__ == "__main__":
+if __name__ == "__main":
     main()
