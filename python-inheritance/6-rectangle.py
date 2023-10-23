4fd5e7a4dee7b09e4000c7bd30cb076c5eef9915
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-23 15:54:56 +0300

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         self.__width = width
-        self.__height = height
+        self.__height = str(height)
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         
