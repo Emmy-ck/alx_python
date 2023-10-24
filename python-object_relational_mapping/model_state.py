4b03a87477aad40665fe9d2b95916e0cf51e00e2
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-24 11:16:13 +0300

    Exporting to json

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 """Start link class to table in the database
 """
+
+
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
