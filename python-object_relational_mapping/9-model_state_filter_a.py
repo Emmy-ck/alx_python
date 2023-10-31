4cd37f9614b4dd6d3f6a62d2dd5be03eaa3c8c73
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-31 23:30:53 +0300

    All states with a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,11 +7,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+# if __name__ == "__main__":
+# Check if the correct number of arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    sys.exit(1)
         
     username = sys.argv[1]
     password = sys.argv[2]
