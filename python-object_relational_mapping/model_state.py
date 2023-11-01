0bfd3dcce0f4c711a69194176897a521f6b9d0c9
Emmy-ck <ckenduiwa9876@gmail.com>
2023-11-01 16:37:37 +0300

    Model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,27 @@
+"""Start link class to table in the database
+"""
+
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class that represents the states table in the database
+    
+    Attributes:
+        id(int): idetity number of each state, unique
+        name(str): name of each state
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
