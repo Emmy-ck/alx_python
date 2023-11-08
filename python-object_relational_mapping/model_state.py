dfce7362ed4f1ac3c284ce0d4cca33f128ef60ad
Emmy-ck <ckenduiwa9876@gmail.com>
2023-11-08 19:18:06 +0300

    Python ORM fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,23 +1,27 @@
 """Start link class to table in the database
 """
 
-from sqlalchemy import Column, Integer, String
+
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-     Maps to the 'states' table.
-
+    Class that represents the states table in the database
+    
     Attributes:
-        id (int): The unique identifier for the state.
-        name (str): The name of the state.
+        id(int): idetity number of each state, unique
+        name(str): name of each state
+    """
 
-    Args:
-        Base (DeclarativeMeta): The base class.
-        """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
